4eba5c4325641123b6ec300491f0e5f300039306
sfarhaazmi <62938911+sfarhaazmi@users.noreply.github.com>
2024-07-06 16:47:37 +0530

    Update Test.java

--- src/main/java/com/example/socialmediaposts/service/Test.java
+++ src/main/java/com/example/socialmediaposts/service/Test.java
@@ -1,13 +1,1 @@
-package com.example.socialmediaposts.service;
 
-public class Test {
-    // [1,2,3,4,5]
-    //sum = 6 ; op = [2,4]
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-
-        }
-    }
-
-}
